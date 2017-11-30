9fcc9489eca908c7ef6ab35f85b05bc539a601cf
zhufeng <358244144@qq.com>
2017-11-30 14:37:42 -0800

    提交项目了

---
+++ Cache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cache
+//
+//  Created by bailing on 2017/11/30.
+//  Copyright © 2017年 bailing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
